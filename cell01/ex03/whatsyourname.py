e4c74a5213f0802c46c58f04ceaf89cff7b01cf3
NOVEMBER19 <68170008@kmitl.ac.th>
2025-06-21 09:38:24 +0000

    finish the rest

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
-fname = input("Hey, what's your name?:")
-lname = input("And your last name?:")
-print ("Well. pleased to meet you,", fname, lname,".")
+fname = input("Hey, what's your name?: ")
+lname = input("And your last name?: ")
+print (f"Well. pleased to meet you, {fname} {lname}.")
