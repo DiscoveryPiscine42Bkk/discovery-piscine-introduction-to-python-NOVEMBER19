08e457f1c2b46a79d303f71e253bdd52abcf3039
NOVEMBER19 <68170008@kmitl.ac.th>
2025-06-22 04:11:03 +0000

    lets finish cell02 before lunch

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+#!/urs/bin/python3
+#cell02 exercise 01 am I negative
+number = int(input("Enter a number: "))
+if number >0:
+    print ("This number is positive.")
+if number <0: 
+    print ("This number is negative.")
+if number == 0:
+    print ("This number is both positive and negative.")
