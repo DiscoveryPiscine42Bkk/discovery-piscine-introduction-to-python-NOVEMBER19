9efca8835af76eac63931ecdcbffc5e442bd2b3a
NOVEMBER19 <68170008@kmitl.ac.th>
2025-06-22 03:30:44 +0000

    finish cell02 as soon as possible

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,7 @@
+#!/urs/bin/python3
+#cell02 exercise 00 am I null
+number = int(input("Enter a number: "))
+if number == 0:
+    print ("The number is equal to zero.")
+else: 
+    print ("This number is different from zero.")
