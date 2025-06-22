08c07b4d05d113f1f1403ca6fb39eb5c8d2036fc
NOVEMBER19 <68170008@kmitl.ac.th>
2025-06-22 04:37:49 +0000

    cell02 should be done soon

---
+++ cell02/ex02/password.py
@@ -0,0 +1,8 @@
+#!/urs/bin/python3
+my_password = "Python is awesome"
+print ("Enter you password.")
+password = input()
+if password == my_password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
