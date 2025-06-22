f3c1313f99693fb68946e8709f39e51feafe9b72
NOVEMBER19 <68170008@kmitl.ac.th>
2025-06-22 09:54:02 +0000

    FixCellEx02

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,5 +1,4 @@
 #!/urs/bin/python3
 response = input("What you gotta say?: ")
-ans = input("I got it! Anything else?: ")
-while not ans == "STOP":
-    ans = input("I got it! Anything else?: ")
+while not response == "STOP":
+    response = input("I got it! Anything else?: ")
