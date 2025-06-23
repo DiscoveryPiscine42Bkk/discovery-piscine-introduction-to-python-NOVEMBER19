201ce667babb16ee88ec39dbcc9ef8f3addb5c01
NOVEMBER19 <68170008@kmitl.ac.th>
2025-06-23 05:28:00 +0000

    only the last one left

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/urs/bin/python3
+
+import array as arr
+
+original_array = [2,8,9,48,8,22,-12,2]
+
+new_array = []
+for number in original_array:
+    new_array.append(number + 2)
+print("Original array:",original_array)
+print("New array:",new_array)
