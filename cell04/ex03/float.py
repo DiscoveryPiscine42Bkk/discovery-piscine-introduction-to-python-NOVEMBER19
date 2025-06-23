c23f9f5a1e78cfd901648c98e6a670f6516d5c97
NOVEMBER19 <68170008@kmitl.ac.th>
2025-06-23 03:55:13 +0000

    i can do this i can reach ex05

---
+++ cell04/ex03/float.py
@@ -0,0 +1,11 @@
+#!/urs/bin/python3
+num = input("Give me a number: ")
+
+number = float(num)
+
+if number.is_integer():
+    print(f"This number is a integer.")
+
+else:
+    print(f"This number is a decimal.")
+
