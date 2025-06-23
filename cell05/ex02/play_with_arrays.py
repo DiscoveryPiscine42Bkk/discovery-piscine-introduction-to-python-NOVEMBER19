cbe087e7e1f81b0eb2b24ac2163e9e758c1f79bd
NOVEMBER19 <68170008@kmitl.ac.th>
2025-06-23 07:10:04 +0000

    im donee

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,5 +1,6 @@
 #!/urs/bin/python3
 original_array = [2,8,9,48,8,22,-12,2]
-processed_array = [x for x in original_array if x > 5]
+processed_array = [num + 2 for num in original_array if num > 5 ]
+
 print(original_array)
 print(processed_array)
