09fd9a7d1412fdd910e4d5d53f7efdd48596fa03
NOVEMBER19 <68170008@kmitl.ac.th>
2025-06-23 05:13:22 +0000

    i did the wrong ex

---
+++ cell05/ex01/parameter_matching.py
@@ -0,0 +1,13 @@
+#!/urs/bin/python3
+
+print("Hello!")
+print("What was the parameter?")
+parameter = "Hello!"
+response = str(input(""))
+noans = ""
+
+if response == parameter:
+    print("Good job!")
+
+else:
+    print("Nope, sorry...")
